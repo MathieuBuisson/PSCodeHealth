151d9bec4d00437a306e847e8ee7431e85f149ed
Mathieu BUISSON <matthieu.buisson@gmail.com>
2017-02-12 19:11:58 +0000

    Validate behaviour of Get-FunctionDefinition when the file doesn't contain any function

--- PSCodeHealthMetrics.psm1
+++ PSCodeHealthMetrics.psm1
@@ -2,20 +2,16 @@
     $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
     $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
-#Dot source the files
     Foreach ( $Import in @($Public + $Private) ) {
-        Try
-        {
+        Try {
             . $Import.FullName
         }
-        Catch
-        {
+        Catch {
             Write-Error -Message "Failed to import function $($Import.FullName): $_"
         }
     }
 
 # Here I might...
-    # Read in or create an initial config file and variable
     # Export Public functions ($Public.BaseName) for WIP modules
     # Set variables visible to the module and its functions only
 
