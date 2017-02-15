d6f3d8860b8d3ca9181c45969b6482f5c811a612
Mathieu BUISSON <matthieu.buisson@gmail.com>
2017-02-15 22:47:07 +0000

    Debug tests failures in Appveyor

--- Tests/Unit/Private/Get-FunctionScriptAnalyzerViolation.Tests.ps1
+++ Tests/Unit/Private/Get-FunctionScriptAnalyzerViolation.Tests.ps1
@@ -1,6 +1,7 @@
 $ModuleName = 'PSCodeHealthMetrics'
 Import-Module "$($PSScriptRoot)\..\..\..\$($ModuleName).psd1" -Force
 
+Get-Module -Name 'Pester'
 Write-Host "PSScriptRoot : $($PSScriptRoot)"
 
 $Mocks = ConvertFrom-Json (Get-Content -Path "$($PSScriptRoot)\..\TestData\MockObjects.json" -Raw )
