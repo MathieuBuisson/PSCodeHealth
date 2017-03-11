5e7832c5ec7b7412d5c49f6a1b08fe829fe40544
Mathieu BUISSON <matthieu.buisson@gmail.com>
2017-03-11 21:50:32 +0000

    Add function to count additional code paths due to If statements

---
+++ PSCodeHealth/Private/Get-FunctionIfCodePath.ps1
@@ -0,0 +1,40 @@
+Function Get-FunctionIfCodePath {
+<#
+.SYNOPSIS
+    Gets the number of additional code paths due to If statements.
+.DESCRIPTION
+    Gets the number of additional code paths due to If statements (including If/Else and If/ElseIf/Else statements), in the specified function definition.
+
+.PARAMETER FunctionDefinition
+    To specify the function definition to analyze.
+
+.EXAMPLE
+    Get-FunctionIfCodePath -FunctionDefinition $MyFunctionAst
+
+    Gets the number of additional code paths due to If statements in the specified function definition.
+
+.OUTPUTS
+    System.Int32
+
+.NOTES
+    General notes
+#>
+    [CmdletBinding()]
+    [OutputType([System.Int32])]
+    Param (
+        [Parameter(Position=0, Mandatory=$True)]
+        [System.Management.Automation.Language.FunctionDefinitionAst]$FunctionDefinition
+    )
+    
+    $FunctionText = $FunctionDefinition.Extent.Text
+
+    # Converting the function definition to a generic ScriptBlockAst because the FindAll method of FunctionDefinitionAst object work strangely
+    $FunctionAst = [System.Management.Automation.Language.Parser]::ParseInput($FunctionText, [ref]$null, [ref]$null)
+    $IfStatements = $FunctionAst.FindAll({ $args[0] -is [System.Management.Automation.Language.IfStatementAst] }, $True)
+
+    If ( -not($IfStatements) ) {
+        return [int]0
+    }
+    # If and ElseIf clauses are creating an additional path, not Else clauses
+    return $IfStatements.Clauses.Count
+}
