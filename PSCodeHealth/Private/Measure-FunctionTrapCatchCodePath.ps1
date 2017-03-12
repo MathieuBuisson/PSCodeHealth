da82cf266f988da7ed1a5e9c16deddb42f6e7497
Mathieu BUISSON <matthieu.buisson@gmail.com>
2017-03-12 18:53:12 +0000

    Add function to count additional code paths due to Trap statements and Catch clauses

---
+++ PSCodeHealth/Private/Measure-FunctionTrapCatchCodePath.ps1
@@ -0,0 +1,38 @@
+Function Measure-FunctionTrapCatchCodePath {
+<#
+.SYNOPSIS
+    Gets the number of additional code paths due to Trap statements and Catch clauses in Try statements.
+.DESCRIPTION
+    Gets the number of additional code paths due to Trap statements and Catch clauses in Try statements, in the specified function definition.
+
+.PARAMETER FunctionDefinition
+    To specify the function definition to analyze.
+
+.EXAMPLE
+    Measure-FunctionTrapCatchCodePath -FunctionDefinition $MyFunctionAst
+
+    Gets the number of additional code paths due to Trap statements and Catch clauses in Try statements, in the specified function definition.
+
+.OUTPUTS
+    System.Int32
+
+.NOTES
+    General notes
+#>
+    [CmdletBinding()]
+    [OutputType([System.Int32])]
+    Param (
+        [Parameter(Position=0, Mandatory=$True)]
+        [System.Management.Automation.Language.FunctionDefinitionAst]$FunctionDefinition
+    )
+    
+    $FunctionText = $FunctionDefinition.Extent.Text
+
+    # Converting the function definition to a generic ScriptBlockAst because the FindAll method of FunctionDefinitionAst object work strangely
+    $FunctionAst = [System.Management.Automation.Language.Parser]::ParseInput($FunctionText, [ref]$null, [ref]$null)
+    $TrapStatements = $FunctionAst.FindAll({ $args[0] -is [System.Management.Automation.Language.TrapStatementAst] }, $True)
+    $CatchClauses = $FunctionAst.FindAll({ $args[0] -is [System.Management.Automation.Language.CatchClauseAst] }, $True)
+
+    [int]$ErrorHandlingCodePaths = $TrapStatements.Count + $CatchClauses.Count
+    return $ErrorHandlingCodePaths
+}
---
+++ Tests/Unit/Private/Measure-FunctionTrapCatchCodePath.Tests.ps1
@@ -0,0 +1,39 @@
+$ModuleName = 'PSCodeHealth'
+Import-Module "$PSScriptRoot\..\..\..\$ModuleName\$($ModuleName).psd1" -Force
+
+Describe 'Measure-FunctionTrapCatchCodePath' {
+    InModuleScope $ModuleName {
+
+        $Mocks = ConvertFrom-Json (Get-Content -Path "$($PSScriptRoot)\..\TestData\MockObjects.json" -Raw )
+
+        Context 'There is no Trap statement or Catch clause in the specified function' {
+
+            $FunctionText = ($Mocks.'Get-FunctionDefinition'.NoTrapOrCatch.Extent.Text | Out-String)
+            $ScriptBlock = [System.Management.Automation.Language.Parser]::ParseInput($FunctionText, [ref]$null, [ref]$null)
+            $FunctionDefinition = $ScriptBlock.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $False)
+            $Result = Measure-FunctionTrapCatchCodePath -FunctionDefinition $FunctionDefinition[0]
+
+            It 'Should return an object of the type [System.Int32]' {
+                $Result | Should BeOfType [System.Int32]
+            }
+            It 'Should return the value 0' {
+                $Result | Should Be 0
+            }
+        }
+
+        Context 'There is a Trap statement and 4 Catch clauses in the specified function' {
+
+            $FunctionText = ($Mocks.'Get-FunctionDefinition'.TrapAndCatch.Extent.Text | Out-String)
+            $ScriptBlock = [System.Management.Automation.Language.Parser]::ParseInput($FunctionText, [ref]$null, [ref]$null)
+            $FunctionDefinition = $ScriptBlock.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $False)
+            $Result = Measure-FunctionTrapCatchCodePath -FunctionDefinition $FunctionDefinition[0]
+
+            It 'Should return an object of the type [System.Int32]' {
+                $Result | Should BeOfType [System.Int32]
+            }
+            It 'Should count the Trap statement and every Catch clauses, including the nested one' {
+                $Result | Should Be 5
+            }
+        }
+    }
+}
