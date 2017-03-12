36e7c2a45572f875d0f55381df1dad8dfd47ef29
Mathieu BUISSON <matthieu.buisson@gmail.com>
2017-03-12 17:32:24 +0000

    Add function to count additional code paths due to logical operators

---
+++ PSCodeHealth/Private/Measure-FunctionLogicalOpCodePath.ps1
@@ -0,0 +1,38 @@
+Function Measure-FunctionLogicalOpCodePath {
+<#
+.SYNOPSIS
+    Gets the number of additional code paths due to Switch statements.
+.DESCRIPTION
+    Gets the number of additional code paths due to Switch statements, in the specified function definition.
+
+.PARAMETER FunctionDefinition
+    To specify the function definition to analyze.
+
+.EXAMPLE
+    Measure-FunctionLogicalOpCodePath -FunctionDefinition $MyFunctionAst
+
+    Gets the number of additional code paths due to Switch statements in the specified function definition.
+
+.OUTPUTS
+    System.Int32
+
+.NOTES
+    General notes
+#>
+    [CmdletBinding()]
+    [OutputType([System.Int32])]
+    Param (
+        [Parameter(Position=0, Mandatory=$True)]
+        [System.Management.Automation.Language.FunctionDefinitionAst]$FunctionDefinition
+    )
+    
+    $FunctionText = $FunctionDefinition.Extent.Text
+    $Tokens = $Null
+    $FunctionAst = [System.Management.Automation.Language.Parser]::ParseInput($FunctionText, [ref]$Tokens, [ref]$Null)
+    $LogicalOperators = $Tokens | Where-Object { $_.Kind.ToString() -In 'And','Or','Xor' }
+
+    If ( -not($LogicalOperators) ) {
+        return [int]0
+    }
+    return $LogicalOperators.Count
+}
---
+++ Tests/Unit/Private/Measure-FunctionLogicalOpCodePath.Tests.ps1
@@ -0,0 +1,39 @@
+$ModuleName = 'PSCodeHealth'
+Import-Module "$PSScriptRoot\..\..\..\$ModuleName\$($ModuleName).psd1" -Force
+
+Describe 'Measure-FunctionLogicalOpCodePath' {
+    InModuleScope $ModuleName {
+
+        $Mocks = ConvertFrom-Json (Get-Content -Path "$($PSScriptRoot)\..\TestData\MockObjects.json" -Raw )
+
+        Context 'There is no logical operator in the specified function' {
+
+            $FunctionText = ($Mocks.'Get-FunctionDefinition'.NoLogicalOp.Extent.Text | Out-String)
+            $ScriptBlock = [System.Management.Automation.Language.Parser]::ParseInput($FunctionText, [ref]$null, [ref]$null)
+            $FunctionDefinition = $ScriptBlock.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $False)
+            $Result = Measure-FunctionLogicalOpCodePath -FunctionDefinition $FunctionDefinition[0]
+
+            It 'Should return an object of the type [System.Int32]' {
+                $Result | Should BeOfType [System.Int32]
+            }
+            It 'Should return the value 0' {
+                $Result | Should Be 0
+            }
+        }
+
+        Context 'There are And, Or and Xor logical operators in the specified function' {
+
+            $FunctionText = ($Mocks.'Get-FunctionDefinition'.LogicalOps.Extent.Text | Out-String)
+            $ScriptBlock = [System.Management.Automation.Language.Parser]::ParseInput($FunctionText, [ref]$null, [ref]$null)
+            $FunctionDefinition = $ScriptBlock.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $False)
+            $Result = Measure-FunctionLogicalOpCodePath -FunctionDefinition $FunctionDefinition[0]
+
+            It 'Should return an object of the type [System.Int32]' {
+                $Result | Should BeOfType [System.Int32]
+            }
+            It 'Should count every logical operator' {
+                $Result | Should Be 3
+            }
+        }
+    }
+}
