b5a6d7f272fcac99c3318bc5b3df93ec1cad898f
Mathieu BUISSON <matthieu.buisson@gmail.com>
2017-03-19 18:22:02 +0000

    Add function to count additional code paths due to While loops

---
+++ PSCodeHealth/Private/Measure-FunctionWhileCodePath.ps1
@@ -0,0 +1,39 @@
+Function Measure-FunctionWhileCodePath {
+<#
+.SYNOPSIS
+    Gets the number of additional code paths due to While statements.
+.DESCRIPTION
+    Gets the number of additional code paths due to While statements, in the specified function definition.
+
+.PARAMETER FunctionDefinition
+    To specify the function definition to analyze.
+
+.EXAMPLE
+    Measure-FunctionWhileCodePath -FunctionDefinition $MyFunctionAst
+
+    Gets the number of additional code paths due to While statements in the specified function definition.
+
+.OUTPUTS
+    System.Int32
+
+.NOTES
+    General notes
+#>
+    [CmdletBinding()]
+    [OutputType([System.Int32])]
+    Param (
+        [Parameter(Position=0, Mandatory=$True)]
+        [System.Management.Automation.Language.FunctionDefinitionAst]$FunctionDefinition
+    )
+    
+    $FunctionText = $FunctionDefinition.Extent.Text
+
+    # Converting the function definition to a generic ScriptBlockAst because the FindAll method of FunctionDefinitionAst object work strangely
+    $FunctionAst = [System.Management.Automation.Language.Parser]::ParseInput($FunctionText, [ref]$null, [ref]$null)
+    $WhileStatements = $FunctionAst.FindAll({ $args[0] -is [System.Management.Automation.Language.WhileStatementAst] }, $True)
+
+    If ( -not($WhileStatements) ) {
+        return [int]0
+    }
+    return $WhileStatements.Count
+}
--- Tests/Unit/Private/Measure-FunctionWhileCodePath.Tests.ps1
+++ Tests/Unit/Private/Measure-FunctionWhileCodePath.Tests.ps1
@@ -1,17 +1,17 @@
 $ModuleName = 'PSCodeHealth'
 Import-Module "$PSScriptRoot\..\..\..\$ModuleName\$($ModuleName).psd1" -Force
 
-Describe 'Measure-FunctionForCodePath' {
+Describe 'Measure-FunctionWhileCodePath' {
     InModuleScope $ModuleName {
 
         $Mocks = ConvertFrom-Json (Get-Content -Path "$($PSScriptRoot)\..\TestData\MockObjects.json" -Raw )
 
-        Context 'There is no For statement in the specified function' {
+        Context 'There is no While statement in the specified function' {
 
-            $FunctionText = ($Mocks.'Get-FunctionDefinition'.NoForStatement.Extent.Text | Out-String)
+            $FunctionText = ($Mocks.'Get-FunctionDefinition'.NoWhileStatement.Extent.Text | Out-String)
             $ScriptBlock = [System.Management.Automation.Language.Parser]::ParseInput($FunctionText, [ref]$null, [ref]$null)
             $FunctionDefinition = $ScriptBlock.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $False)
-            $Result = Measure-FunctionForCodePath -FunctionDefinition $FunctionDefinition[0]
+            $Result = Measure-FunctionWhileCodePath -FunctionDefinition $FunctionDefinition[0]
 
             It 'Should return an object of the type [System.Int32]' {
                 $Result | Should BeOfType [System.Int32]
@@ -21,17 +21,17 @@ Describe 'Measure-FunctionForCodePath' {
             }
         }
 
-        Context 'There are 3 For statements, including 1 nested and 1 with no condition' {
+        Context 'There are 2 While statements, including 1 nested' {
 
-            $FunctionText = ($Mocks.'Get-FunctionDefinition'.ForStatements.Extent.Text | Out-String)
+            $FunctionText = ($Mocks.'Get-FunctionDefinition'.WhileStatements.Extent.Text | Out-String)
             $ScriptBlock = [System.Management.Automation.Language.Parser]::ParseInput($FunctionText, [ref]$null, [ref]$null)
             $FunctionDefinition = $ScriptBlock.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst] }, $False)
-            $Result = Measure-FunctionForCodePath -FunctionDefinition $FunctionDefinition[0]
+            $Result = Measure-FunctionWhileCodePath -FunctionDefinition $FunctionDefinition[0]
 
             It 'Should return an object of the type [System.Int32]' {
                 $Result | Should BeOfType [System.Int32]
             }
-            It 'Should count every For statements, except for the one which has no condition' {
+            It 'Should count every While statements, including the nested one' {
                 $Result | Should Be 2
             }
         }
