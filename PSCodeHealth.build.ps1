df2e691ec81451e83950cac595b94ec62ad32366
Mathieu BUISSON <matthieu.buisson@gmail.com>
2017-04-30 14:42:22 +0100

    Add tasks 'Fail_If_Failed_Unit_Test' and 'Copy_Source_To_Build_Output' for Invoke-Build

--- PSCodeHealth.build.ps1
+++ PSCodeHealth.build.ps1
@@ -2,28 +2,30 @@
 Param (
     [string]$BuildOutput = "$PSScriptRoot\BuildOutput",
 
-    [string[]]$Dependency = @('Coveralls','Pester','PsScriptAnalyzer')
+    [string[]]$Dependency = @('Coveralls','Pester','PsScriptAnalyzer'),
+
+    [string]$SourceFolder = "$PSScriptRoot\$($env:APPVEYOR_PROJECT_NAME)"
 )
 
 Function Write-TaskBanner ( [string]$TaskName )
 {
-    "`n" + ('-' * 79) + "`n" + "`t`t`t $TaskName `n" + ('-' * 79) + "`n"
+    "`n" + ('-' * 79) + "`n" + "`t`t`t $($TaskName.ToUpper()) `n" + ('-' * 79) + "`n"
 }
 
 task Clean {
     Write-TaskBanner -TaskName $Task.Name
 
-    If (Test-Path -Path $BuildOutput) {
-        "Removing existing files and folders in $BuildOutput\"
+    If (Test-Path -Path $Script:BuildOutput) {
+        "Removing existing files and folders in $($Script:BuildOutput)\"
         Get-ChildItem .\BuildOutput\ | Remove-Item -Force -Recurse
     }
     Else {
-        "$BuildOutput is not present, nothing to clean up."
-        $Null = New-Item -ItemType Directory -Path $BuildOutput
+        "$Script:BuildOutput is not present, nothing to clean up."
+        $Null = New-Item -ItemType Directory -Path $Script:BuildOutput
     }
 }
 
-task InstallDependencies {
+task Install_Dependencies {
     Write-TaskBanner -TaskName $Task.Name
 
     Foreach ( $Depend in $Script:Dependency ) {
@@ -32,11 +34,11 @@ task InstallDependencies {
     }
 }
 
-task UnitTests {
+task Unit_Tests {
     Write-TaskBanner -TaskName $Task.Name
 
     $UnitTestParams = @{
-        Script = '.\Tests'
+        Script = '.\Tests\Unit'
         CodeCoverage = '.\PSCodeHealth\P*\*'
         OutputFile = 'UnitTestsResult.xml'
         PassThru = $True
@@ -44,5 +46,19 @@ task UnitTests {
     $Script:UnitTestsResult = Invoke-Pester @UnitTestParams
 }
 
-# Default task : runs all tasks
-task . Clean, InstallDependencies, UnitTests
+task Fail_If_Failed_Unit_Test -If ( $Script:UnitTestsResult.FailedCount -ne 0 ) {
+    Write-TaskBanner -TaskName $Task.Name
+
+    assert ($Script:UnitTestsResult.FailedCount -eq 0) ('{0} Unit test(s) failed. Aborting build' -f $Script:UnitTestsResult.FailedCount)
+}
+
+Task Copy_Source_To_Build_Output {
+    Write-TaskBanner -TaskName $Task.Name
+
+    "Copying the source directory [$Script:SourceFolder] into the build output folder : [$Script:BuildOutput]"
+    Copy-Item -Path $Script:SourceFolder -Destination $Script:BuildOutput -Recurse
+}
+
+
+# Default task :
+task . Clean, Install_Dependencies, Unit_Tests, Fail_If_Failed_Unit_Test, Copy_Source_To_Build_Output
